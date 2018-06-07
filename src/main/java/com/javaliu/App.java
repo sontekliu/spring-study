afbed045d943e7cd7dd13904db7f854cd0271a2b
sontek <sontek@yeah.net>
2018-06-07 09:43:32 +0800

    init project

---
+++ src/main/java/com/javaliu/App.java
@@ -0,0 +1,13 @@
+package com.javaliu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
