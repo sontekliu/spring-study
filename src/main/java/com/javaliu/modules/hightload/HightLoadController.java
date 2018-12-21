648135e91b926078be4fc04c00d62fa29e8976e7
sontek <sontek@yeah.net>
2018-12-21 17:40:02 +0800

    update high

--- src/main/java/com/javaliu/modules/hightload/HightLoadController.java
+++ src/main/java/com/javaliu/modules/hightload/HightLoadController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping(value = "/highLoad/")
@@ -17,7 +17,7 @@ public class HightLoadController {
 
     private static final Logger logger = LoggerFactory.getLogger(HightLoadController.class);
 
-    private List<byte[]> list = new ArrayList<>();
+    private static final List<String> list = new ArrayList<>();
 
     @RequestMapping(value = "highCPU")
     public void highCPU(){
@@ -27,29 +27,14 @@ public class HightLoadController {
     }
 
     @RequestMapping(value = "highMemory")
-    public String highMemory(HttpServletRequest request, HttpServletResponse response){
-        for(int i=0; i< 5;i++){
-            File file = new File("/opt/software/jdk-8u191-linux-x64.tar.gz");
-            InputStream inputStream = null;
-            try {
-                inputStream = new BufferedInputStream(new FileInputStream(file));
-                byte[] buffer = new byte[1024 * 1000];
-                while (inputStream.read(buffer) != -1){
-                    list.add(buffer);
-                    buffer = new byte[1024 * 1000];
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                try {
-                    if (null != inputStream) {
-                        inputStream.close();
-                    }
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
+    public void highMemory(HttpServletRequest request, HttpServletResponse response){
+        for (int i=0;i<1000000;i++){
+           String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+           StringBuffer sb = new StringBuffer();
+           for (int j=0;j<1000;j++){
+               sb.append(str.charAt(new Random().nextInt(61)));
+           }
+           list.add(sb.toString());
         }
-        return "index";
     }
 }
